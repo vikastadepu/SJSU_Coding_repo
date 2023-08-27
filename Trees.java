f880975098728ee8aea1b517d8a2211624b1a61f
vikastadepu <vikastadepuffe@gmail.com>
2023-08-27 10:02:30 -0700

    Diameter of a tree

---
+++ Trees.java
@@ -0,0 +1,21 @@
+
+class Solution {
+    //int ans=0;
+    public int maxDepth(TreeNode root) 
+    {
+        int[] ans=new int[1];
+        return helper(root,0,ans);
+      
+    }
+
+    public int helper(TreeNode root, int d,int[] ans)
+    {
+        if(root==null)
+        {
+            ans[0]=Math.max(ans[0],d);
+            return ans[0];
+        }
+        return Math.max(helper(root.left,d+1,ans),helper(root.right,d+1,ans));
+        
+    }
+}
